7905caeae9503e6adf65359c029cce0e29eddcc6
vitoranello <vitoranello@gmail.com>
2025-05-19 17:13:49 -0300

    Refact speech_synthesizer

--- fbot_speech/fbot_speech_nodes/speech_synthesizer.py
+++ fbot_speech/fbot_speech_nodes/speech_synthesizer.py
@@ -1,63 +1,60 @@
 #!/usr/bin/env python3
 # coding: utf-8
-import os
 import numpy as np
 import riva.client
-import rospkg
-import warnings
 
-from scipy.io import wavfile
-from termcolor import colored
-from std_srvs.srv import SetBool
-from fbot_speech_msgs.srv import AudioPlayer, AudioPlayerByData, SynthesizeSpeech
+from fbot_speech_msgs.srv import AudioPlayerByData, SynthesizeSpeech
 from fbot_speech_msgs.msg import SynthesizeSpeechMessage
 from audio_common_msgs.msg import AudioData, AudioInfo
 import rclpy
 from rclpy.node import Node
 
-# Get the path of the 'fbot_speech' package
-#PACK_DIR = rospkg.RosPack().get_path("fbot_speech")
-# Construct the path to the audio directory
-#AUDIO_DIR = os.path.join(PACK_DIR, "audios/")
-# Set the filename for the generated audio file
-#FILENAME = str(AUDIO_DIR) + "talk.wav"
-
 class SpeechSynthesizerNode(Node):
     def __init__(self):
         super().__init__('speech_synthesizer')
-        
         self.get_logger().info("Initializing Speech Synthesizer Node...")
-        
-        # Parameters
-        self.config_defaults = {
-            "language_code": "en-US",
-            "sample_rate_hz": 44100,
-            "voice_name": "English-US.Male-1",
-        }
-        
-        self.configs = self.get_parameter_or("tts_configs", self.config_defaults)
-        
-        self.riva_url = self.get_parameter_or("riva/url", "localhost:50051")
+        self.declareParameters()
+        self.readParameters()
+        self.init_rosComm()
         auth = riva.client.Auth(uri=self.riva_url)
         self.riva_tts = riva.client.SpeechSynthesisService(auth)
-
-        # Fetch the audio player by data service parameter
-        self.audio_player_by_data_service_param = self.get_parameter_or("services/audio_player_by_data/service", "/fbot_speech/ap/audio_player_by_data")
-
-        # Subscribe to a topic for text input
-        self.synthesizer_subscriber_param = self.get_parameter_or("subscribers/speech_synthesizer/topic", "/fbot_speech/ss/say_something")
-        self.create_subscription(SynthesizeSpeechMessage, self.synthesizer_subscriber_param, self.synthesizeSpeechCallback, 10)
-
-        # Create the service for speech synthesis
-        self.synthesizer_service_param = self.get_parameter_or("services/speech_synthesizer/service", "/fbot_speech/ss/say_something")
-        self.create_service(SynthesizeSpeech, self.synthesizer_service_param, self.synthesizeSpeech)
-
         self.get_logger().info("Speech Synthesizer Node initialized!")
 
+    def init_rosComm(self):
+        super().__init__(self)
+        # Create subscriber
+        self.synthesizerSubscriber = self.create_subscription(SynthesizeSpeechMessage, self.synthesizer_subscriber_param, self.synthesizeSpeechCallback, 10)
+        # Create service
+        self.synthesizerService = self.create_service(SynthesizeSpeech, self.synthesizer_service_param, self.synthesizeSpeech)        
+
+    def declareParameters(self):
+        self.declare_parameter('tts_configs.language_code', 'en-US')
+        self.declare_parameter('tts_configs.sample_rate_hz', 44100)
+        self.declare_parameter('tts_configs.voice_name', 'English-US')
+        self.declare_parameter('riva.url', 'localhost:50051')
+        self.declare_parameter('services.audio_player_by_data.service', '/fbot_speech/ap/audio_player_by_data')
+        self.declare_parameter('services.speech_synthesizer.service', '/fbot_speech/ss/say_something')
+        self.declare_parameter('subscribers.speech_synthesizer.topic', '/fbot_speech/ss/say_something')
+
+    def readParameters(self):
+        self.audio_player_by_data_service_param = self.get_parameter('services.audio_player_by_data.service').get_parameter_value().string_value
+        self.synthesizer_service_param = self.get_parameter('services.speech_synthesizer.service').get_parameter_value().string_value
+        self.synthesizer_subscriber_param = self.get_parameter('subscribers.speech_synthesizer.topic').get_parameter_value().string_value
+        self.configs = {
+            "language_code": self.get_parameter('tts_configs.language_code').get_parameter_value().string_value,
+            "sample_rate_hz": self.get_parameter('tts_configs.sample_rate_hz').get_parameter_value().integer_value,
+            "voice_name": self.get_parameter('tts_configs.voice_name').get_parameter_value().string_value,
+        }
+        self.riva_url = self.get_parameter('riva.url').get_parameter_value().string_value
+
     def synthesizeSpeech(self, request: SynthesizeSpeech.Request, response: SynthesizeSpeech.Response):
+        """
+        @brief Synthesize speech from text using Riva TTS.
+        @param request: The request object containing the text to synthesize.
+        @return: The response object indicating success or failure.
+        """
         config = self.configs
         speech = request.text
-        
         try:
             # Call Riva TTS to synthesize the speech
             resp = self.riva_tts.synthesize(
@@ -71,7 +68,6 @@ class SpeechSynthesizerNode(Node):
             
             # Convert the response audio to a NumPy array
             audio_samples = np.frombuffer(resp.audio, dtype=np.int16)
-
             # Prepare the audio data to send to the audio player
             audio_data = AudioData()
             audio_data.uint8_data = audio_samples.tobytes()
@@ -94,7 +90,6 @@ class SpeechSynthesizerNode(Node):
             audio_player_request.audio_info = audio_info
             future = audio_player_client.call_async(audio_player_request)
             future.result()
-
             response.success = True
             self.get_logger().info(f"Audio data played successfully.")
         
@@ -105,18 +100,21 @@ class SpeechSynthesizerNode(Node):
         return response
 
     def synthesizeSpeechCallback(self, msg: SynthesizeSpeechMessage):
+        """
+        @brief Callback function for the speech synthesizer subscriber.
+        This function is called when a new message is received on the subscriber topic. It extracts the text and language from the message and calls the synthesizeSpeech function.
+        @param msg: The message object containing the text and language to synthesize.
+        """
         request = SynthesizeSpeech.Request()
         request.text = msg.text
         request.lang = msg.lang
         self.synthesizeSpeech(request, SynthesizeSpeech.Response())
-
 
 def main(args=None):
     rclpy.init(args=args)
     node = SpeechSynthesizerNode()
     rclpy.spin(node)
     rclpy.shutdown()
-
 
 if __name__ == '__main__':
     main()
