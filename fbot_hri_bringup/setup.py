62933b2f91facfc82bee79c538d8acd029b7c7b5
Vitor Anello <93740812+vitoranello@users.noreply.github.com>
2025-07-06 14:43:28 -0300

    Update setup.py

--- fbot_hri_bringup/setup.py
+++ fbot_hri_bringup/setup.py
@@ -21,6 +21,7 @@ setup(
     maintainer_email='stihl-bolsistas@todo.todo',
     description='TODO: Package description',
     license='TODO: License declaration',
+    tests_require=['pytest'],
     entry_points={
         'console_scripts': [
         ],
