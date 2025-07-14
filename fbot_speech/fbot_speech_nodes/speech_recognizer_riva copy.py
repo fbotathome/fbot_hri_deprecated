0cc97bba81aede3327b10fd2e918082b81c1657d
vitoranello <vitoranello@gmail.com>
2025-07-14 03:40:30 -0300

    Adjust small features

--- fbot_speech/fbot_speech_nodes/speech_recognizer_riva copy.py
+++ fbot_speech/fbot_speech_nodes/speech_recognizer_riva copy.py
@@ -31,62 +31,107 @@ class SpeechRecognizerNode(Node):
         auth = riva.client.Auth(uri=self.riva_url)
         self.riva_asr = riva.client.ASRService(auth)
         self.config = riva.client.StreamingRecognitionConfig(
-            config = riva.client.RecognitionConfig(
-                encoding=riva.client.AudioEncoding.LINEAR_PCM,
-                language_code='en-US',
-                model=args.model_name,
-                max_alternatives=1,
-                profanity_filter=False,
-                enable_automatic_punctuation=False,
-                verbatim_transcripts=True,
-                sample_rate_hertz=args.sample_rate_hz,
-                audio_channel_count=1,
-                enable_word_time_offsets = True,
+                                                            config = riva.client.RecognitionConfig(
+                                                                    encoding=riva.client.AudioEncoding.LINEAR_PCM,
+                                                                    language_code='en-US',
+                                                                    max_alternatives=1,
+                                                                    profanity_filter=False,
+                                                                    enable_automatic_punctuation=False,
+                                                                    verbatim_transcripts=False,
+                                                                    sample_rate_hertz=16000,
+                                                                    audio_channel_count=1,
+                                                                ),
+                                                            interim_results=False)
 
-            ),
-            interim_results=True
+        riva.client.add_endpoint_parameters_to_config(
+            self.config,
+            start_history= 100,
+            start_threshold= -1,
+            stop_history= 500,
+            stop_history_eou= 700,
+            stop_threshold= -1.0,
+            stop_threshold_eou= -1.0,
         )
 
+        default_device_info = riva.client.audio_io.get_default_input_device_info()
+        self.device = default_device_info['index']
 
-        speech_context = rasr.SpeechContext()
-        speech_context.phrases.extend(boosted_lm_words)
-        speech_context.boost = boosted_lm_score
-        self.config.speech_contexts.append(speech_context)
+        self.word = True
+        self.sentence = not self.word
 
-
-        endpointing_config = rasr.EndpointingConfig()
-        if start_history > 0:
-            endpointing_config.start_history = start_history
-        if start_threshold > 0:
-            endpointing_config.start_threshold = start_threshold
-        if stop_history > 0:
-            endpointing_config.stop_history = stop_history
-        if stop_history_eou > 0:
-            endpointing_config.stop_history_eou = stop_history_eou
-        if stop_threshold > 0:
-            endpointing_config.stop_threshold = stop_threshold
-        if stop_threshold_eou > 0:
-            endpointing_config.stop_threshold_eou = stop_threshold_eou
-        self.config.endpointing_config.CopyFrom(endpointing_config)
-
-    def delayStarterRecorder(self):
         
 
     def handleRecognition(self, req: SpeechToText.Request, response: SpeechToText.Response):
-        config = self.configs
-        speech = request.text
-        try:
-             with riva.client.audio_io.MicrophoneStream(
-                args.sample_rate_hz,
-                args.file_streaming_chunk,
-                device=args.input_device,
-            ) as audio_chunk_iterator:
-                riva.client.print_streaming(
-                    responses=self.riva_asr.streaming_response_generator(
-                        audio_chunks=audio_chunk_iterator,
-                        streaming_config=config,
-                    )
-                )
+        with riva.client.audio_io.MicrophoneStream(
+                                                        rate =16000,
+                                                        chunk=512,
+                                                        device=self.device,
+                                                    ) as audio_chunk_iterator:
+            speech_context = rasr.SpeechContext()
+
+            resultado_muito_ruim = ''
+            resultado_bom = ''
+            resultado_ruim = ''
+
+            self.restaurant = ['coke', 'pizza', 'wine', 'water', 'sandwich']
+
+            self.boosted_lm_words = ['James', 'Michael', 'Robert', 'John', 'David', 'William', 'Richard', 'Joseph', 'Thomas',
+                            'Christopher', 'Mary',  'Patricia', 'Jennifer', 'Linda', 'Elizabeth', 'Barbara', 'Susan', 'Jessica', 'Karen', 'Sarah'] + self.restaurant
+
+            words = ['Vitor', 'Andrei', 'Richard', 'PEPSI', 'Rice'] + self.boosted_lm_words
+            boosted = 100
+
+
+            speech_context.phrases.extend(words)
+            speech_context.boost = boosted
+            self.config.config.speech_contexts.clear()
+            self.config.config.speech_contexts.extend([speech_context])
+            
+            bad_output = ''
+            very_bad_output = ''
+
+
+            output = self.asr_service.streaming_response_generator(
+                    audio_chunks=audio_chunk_iterator,
+                    streaming_config=self.config)
+            
+            start = time.time() + self.timeout
+            for response in output:
+                if (start > time.time()):
+                    if self.word:
+                        if not response.results:
+                            continue
+                        for result in response.results:
+                            if not result.alternatives:
+                                continue
+                        if result.is_final:
+                            if result.alternatives[0].words[0].word in words:
+                                if result.alternatives[0].words[0].confidence >=0.6:
+                                    resultado_bom = result.alternatives[0].words[0].word
+                                    return resultado_bom
+                                else:
+                                    resultado_ruim = result.alternatives[0].words[0].word
+                            else:
+                                resultado_muito_ruim = result.alternatives[0].words[0].word
+                    elif self.sentence:
+                        if not response.results:
+                            continue
+                        for result in response.results:
+                            if not result.alternatives:
+                                continue
+                        if result.is_final:
+                            for alternative in result.alternatives:
+                                for word in alternative.words:
+                                    if word.word in words:
+                                        return result.alternatives[0].transcript 
+                                    else:
+                                        resultado_ruim = result.alternatives[0].transcript
+                else:
+                    if resultado_ruim != '':
+                        return resultado_ruim
+                    else:
+                        return resultado_muito_ruim
+
 
 def main(args=None):
     rclpy.init(args=args)
